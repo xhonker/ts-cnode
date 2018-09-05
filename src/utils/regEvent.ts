0550a992e0f11b891d794751ddaf2d2b2e36ca43
wuyuanqiang <wuyuanqiang.lanyife.com>
2018-09-05 13:45:41 +0800

    fix:修复用户跳转

--- src/utils/regEvent.ts
+++ src/utils/regEvent.ts
@@ -7,11 +7,12 @@ export const regEvent = () => {
   document.querySelector("#app")!.addEventListener("click", e => {
     let ev = e || window.event;
     let target = ev.target;
+    // @ts-ignore
+    let isRelativeURL = target.host === window.location.host;
     //@ts-ignore
     if (target.nodeName.toLowerCase() === "a") {
       // @ts-ignore
-      if (~target.textContent.indexOf("@")) {
-        // 如果是用户 则阻止跳转
+      if (isRelativeURL && ~target.textContent.indexOf("@")) {
         // @ts-ignore
         let user = target.textContent.replace("@", ""); // @xhonker
         e.preventDefault();
