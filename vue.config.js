169cd6d1a80fe1739aa10b25c8e8f55e1a1c0f3c
xhonker <wu_xhonker@hotmail.com>
2018-09-06 18:25:35 +0800

    fix: image to base64

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,19 @@
 module.exports = {
   baseUrl: "./",
-  assetsDir: "./"
+  assetsDir: "./",
+  chainWebpack: config => {
+    config.module
+      .rule("images")
+      .use("url-loader")
+      .loader("url-loader")
+      .options({
+        limit: 8192,
+        fallback: {
+          loader: "file-loader",
+          options: {
+            name: "img/[name].[hash:8].[ext]"
+          }
+        }
+      });
+  }
 };
